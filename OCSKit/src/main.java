731f725eaf6cfb27a3de50d9b8843fc28d434a9f
Siwat Sirichai <siwat@siwatinc.com>
2019-11-21 13:32:26 +0700

    add Setup Screen

---
+++ OCSKit/src/main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.beans.Visibility;
+public class main {
+    public static void main(String args[]){
+        JFrame frame = new JFrame("Setup");
+        //frame.setContentPane(new Setup().contentpane);
+        frame.setContentPane(new Setup().$$$getRootComponent$$$());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setTitle("OSCSuite | Siwat INC");
+        frame.setVisible(true);
+    }
+}
